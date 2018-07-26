9671366bcbf15e43b6aafae99a90255ba825c5be
Kumar, Ranjeet <ranjeet.kumar@rakuten.com>
2018-07-27 00:34:49 +0530

    initial setup

---
+++ src/main/java/com/saveme/flight/App.java
@@ -0,0 +1,9 @@
+package com.saveme.flight;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Save me");
+	}
+
+}
---
+++ src/test/java/com/saveme/flight/AppTest.java
@@ -0,0 +1,6 @@
+package com.saveme.flight;
+
+
+class AppTest {
+
+}
